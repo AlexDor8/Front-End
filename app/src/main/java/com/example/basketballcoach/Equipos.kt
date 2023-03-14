62ba2e6d321a1442bc213e324f377ab8dfbea109
AlexDor8 <st2022273@365.stucom.com>
2023-03-14 16:24:22 +0100

    recyclerview hecho, falta recyclerview retrofit

--- app/src/main/java/com/example/basketballcoach/Equipos.kt
+++ app/src/main/java/com/example/basketballcoach/Equipos.kt
@@ -2,6 +2,10 @@ package com.example.basketballcoach
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.basketballcoach.adapterTeam.JugadoresAdapter
+import com.example.basketballcoach.adapterTeams.EquiposAdapter
 import com.example.basketballcoach.model.Equipo
 import com.example.basketballcoach.model.Jugador
 
@@ -13,8 +17,21 @@ class Equipos : AppCompatActivity() {
         Equipo(3, "Orlando Magic",1, "")
     )
 
+    private lateinit var equiposRvAdapter: EquiposAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_equipos)
+
+        inicializacionRecyclerView()
+
+    }
+
+    private fun inicializacionRecyclerView() {
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerEquipos)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        equiposRvAdapter = EquiposAdapter(listaEquipos)
+        recyclerView.adapter = equiposRvAdapter
+
     }
 }
