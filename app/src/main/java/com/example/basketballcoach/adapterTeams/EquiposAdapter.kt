b61214be7754f094dfd19f8f4ef9755768d04761
AlexDor8 <st2022273@365.stucom.com>
2023-03-14 16:07:58 +0100

    preparado recyclerView equipo

--- app/src/main/java/com/example/basketballcoach/adapterTeams/EquiposAdapter.kt
+++ app/src/main/java/com/example/basketballcoach/adapterTeams/EquiposAdapter.kt
@@ -1,20 +1,25 @@
 package com.example.basketballcoach.adapterTeams
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.basketballcoach.R
+import com.example.basketballcoach.adapterTeam.JugadoresViewHolder
 import com.example.basketballcoach.model.Equipo
 
 
 class EquiposAdapter(val listEquipos: List<Equipo>): RecyclerView.Adapter<EquiposViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EquiposViewHolder {
-        TODO("Not yet implemented")
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return EquiposViewHolder(layoutInflater.inflate(R.layout.item_equipo, parent, false))
     }
 
     override fun onBindViewHolder(holder: EquiposViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = listEquipos[position]
+        holder.itemPorListado(item)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return listEquipos.size
     }
 }
--- app/src/main/java/com/example/basketballcoach/adapterTeams/EquiposViewHolder.kt
+++ app/src/main/java/com/example/basketballcoach/adapterTeams/EquiposViewHolder.kt
@@ -1,7 +1,22 @@
 package com.example.basketballcoach.adapterTeams
 
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.basketballcoach.R
+import com.example.basketballcoach.model.Equipo
+import com.example.basketballcoach.model.Jugador
 
 class EquiposViewHolder(view: View): RecyclerView.ViewHolder(view) {
+
+    val fotoEquipo = view.findViewById<ImageView>(R.id.fotoEquipoRV)
+    val nombreEquipo = view.findViewById<TextView>(R.id.nombreEquipoRV)
+
+    fun itemPorListado(equipo: Equipo) {
+        nombreEquipo.text = equipo.nombre
+        Glide.with(fotoEquipo.context).load(equipo.foto).into(fotoEquipo)
+    }
+
 }
