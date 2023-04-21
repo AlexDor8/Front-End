eaf3dda5058648f300bb7b68c19d371c9dfd1641
AlexDor8 <st2022273@365.stucom.com>
2023-04-21 16:50:09 +0200

    alineacion acabada y scrollview jugadores

--- app/src/main/java/com/example/basketballcoach/Alineacion.kt
+++ app/src/main/java/com/example/basketballcoach/Alineacion.kt
@@ -27,7 +27,17 @@ class Alineacion : AppCompatActivity() {
         "SF"
     )
 
+    var listaJugadoresNombreEscolta = mutableListOf<String>(
+        "SG"
+    )
 
+    var listaJugadoresNombreAlaPivot = mutableListOf<String>(
+        "PF"
+    )
+
+    var listaJugadoresNombrePivot = mutableListOf<String>(
+        "C"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,9 +45,15 @@ class Alineacion : AppCompatActivity() {
 
         conexion()
         conexionAlero()
+        conexionEscolta()
+        conexionAlaPivot()
+        conexionPivot()
 
         confSpinnerBase()
         confSpinnerAlero()
+        confSpinnerEscolta()
+        confSpinnerAlapivot()
+        confSpinnerPivot()
 
     }
 
@@ -57,13 +73,42 @@ class Alineacion : AppCompatActivity() {
         val spinnerAlero = findViewById<Spinner>(R.id.spinnerAlero)
         val adaptador = ArrayAdapter(this, android.R.layout.simple_spinner_item, listaJugadoresNombreAlero)
 
-
-
         adaptador.setDropDownViewResource(
             android.R.layout.simple_spinner_dropdown_item
         )
 
         spinnerAlero.adapter = adaptador
+    }
+
+    private fun confSpinnerEscolta() {
+        val spinnerEscolta = findViewById<Spinner>(R.id.spinnerEscolta)
+        val adaptador = ArrayAdapter(this, android.R.layout.simple_spinner_item, listaJugadoresNombreEscolta)
+
+        adaptador.setDropDownViewResource(
+            android.R.layout.simple_spinner_dropdown_item
+        )
+
+        spinnerEscolta.adapter = adaptador
+    }
+
+    private fun confSpinnerAlapivot() {
+        val spinnerAlapivot = findViewById<Spinner>(R.id.spinnerAlapivot)
+        val adaptador = ArrayAdapter(this, android.R.layout.simple_spinner_item, listaJugadoresNombreAlaPivot)
+
+        adaptador.setDropDownViewResource(
+            android.R.layout.simple_spinner_dropdown_item
+        )
+        spinnerAlapivot.adapter = adaptador
+    }
+
+    private fun confSpinnerPivot() {
+        val spinnerPivot = findViewById<Spinner>(R.id.spinnerPivot)
+        val adaptador = ArrayAdapter(this, android.R.layout.simple_spinner_item, listaJugadoresNombrePivot)
+
+        adaptador.setDropDownViewResource(
+            android.R.layout.simple_spinner_dropdown_item
+        )
+        spinnerPivot.adapter = adaptador
     }
 
     private fun conexion() {
@@ -99,6 +144,63 @@ class Alineacion : AppCompatActivity() {
                     listaJugadoresNombreAlero.clear();
                     listaJugadoresNombreAlero.addAll(nuevosJugadores)
                     println(listaJugadoresNombreAlero)
+                }
+            }
+        }
+    }
+
+    private fun conexionEscolta() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val interceptor = HttpLoggingInterceptor()
+            interceptor.level = HttpLoggingInterceptor.Level.BODY
+            val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+            val conexion = Retrofit.Builder().baseUrl("http://10.0.2.2:8081/").addConverterFactory(
+                GsonConverterFactory.create()).client(client).build()
+            var respuesta = conexion.create(APIService::class.java).getNombresPosicion("baloncesto/buscarNombrePosicion/${Globals.equipo.id}/Escolta")
+            withContext(Dispatchers.Main) {
+                if (respuesta.isSuccessful) {
+                    val nuevosJugadores = respuesta.body() ?: emptyList()
+                    listaJugadoresNombreEscolta.clear();
+                    listaJugadoresNombreEscolta.addAll(nuevosJugadores)
+                    println(listaJugadoresNombreEscolta)
+                }
+            }
+        }
+    }
+
+    private fun conexionAlaPivot() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val interceptor = HttpLoggingInterceptor()
+            interceptor.level = HttpLoggingInterceptor.Level.BODY
+            val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+            val conexion = Retrofit.Builder().baseUrl("http://10.0.2.2:8081/").addConverterFactory(
+                GsonConverterFactory.create()).client(client).build()
+            var respuesta = conexion.create(APIService::class.java).getNombresPosicion("baloncesto/buscarNombrePosicion/${Globals.equipo.id}/Ala-pivot")
+            withContext(Dispatchers.Main) {
+                if (respuesta.isSuccessful) {
+                    val nuevosJugadores = respuesta.body() ?: emptyList()
+                    listaJugadoresNombreAlaPivot.clear();
+                    listaJugadoresNombreAlaPivot.addAll(nuevosJugadores)
+                    println(listaJugadoresNombreAlaPivot)
+                }
+            }
+        }
+    }
+
+    private fun conexionPivot() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val interceptor = HttpLoggingInterceptor()
+            interceptor.level = HttpLoggingInterceptor.Level.BODY
+            val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+            val conexion = Retrofit.Builder().baseUrl("http://10.0.2.2:8081/").addConverterFactory(
+                GsonConverterFactory.create()).client(client).build()
+            var respuesta = conexion.create(APIService::class.java).getNombresPosicion("baloncesto/buscarNombrePosicion/${Globals.equipo.id}/Pivot")
+            withContext(Dispatchers.Main) {
+                if (respuesta.isSuccessful) {
+                    val nuevosJugadores = respuesta.body() ?: emptyList()
+                    listaJugadoresNombrePivot.clear();
+                    listaJugadoresNombrePivot.addAll(nuevosJugadores)
+                    println(listaJugadoresNombrePivot)
                 }
             }
         }
